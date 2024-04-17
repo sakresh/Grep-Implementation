0bcb993ec32d0f80d09e7382930d0ed225e4f381
name <sarkeshnandu@gmail.com>
2024-04-17 20:25:54 +0530

    Changed mind to use clap instead of using the env

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,3 @@
-use std::fs;
-use std::io;
-
-fn main() {
-    let file = fs::read_to_string("file.txt").expect("Unable to read contents of the file");
-    let mut content = String::new();
-    io::stdin().read_line(&mut content).expect("Unabel to read input");
-    println!("The contents of the file are, \n{}", file);
+fn main(){
+    println!("Grep");
 }
