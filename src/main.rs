cd182458a2733949277dc5332c4e41c500cde17f
name <sarkeshnandu@gmail.com>
2024-04-26 20:06:22 +0530

    Working with file handling rather than command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main(){
-    println!("Grep");
+use std::fs;
+// use std::io;
+
+fn main() {
+    let file = fs::read_to_string("file.txt").unwrap();
+    // let mut content = String::new();
+    let content = "file";
+    for item in file.lines(){
+        println!("{}", item);
+        // if item == content{
+        //     println!("{} \n", item);
+        // }
+    }
+    // io::stdin().read_line(&mut content).expect("Unabel to read input");
+    // println!("The contents of the file are, \n{file}");
 }
